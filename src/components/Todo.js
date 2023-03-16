6b7e6846bf456b75cbd4bab7a4fed0674225af1f
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-17 01:47:23 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,66 @@
-import React from 'react';
+ import React, { useEffect, useState } from 'react';
 
-export const Todo = (props) => {
+ export const Todo  = (props) =>{
 
-    return (
-        <li>
-            <div className="todo" id={props.id}>
-                <div className="todo-text">{props.title}</div>
-                <div className="todo-status">{props.completed ? "Completed" : "InCompleted"}</div>
-            </div>
-        </li>
+    const {dataFetched, setDataFetched} = props;
+    const [isCompleted, setIsCompleted] = useState(true);
+    const [isIncompleted, setIsIncompleted] = useState(true);
+
+    const [fetchedData, setFetchedData] = useState([]);
+
+    useEffect(() => {
+        const fetchTodo = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+            const json = await response.json();
+            setFetchedData(json);
+            console.log(json);
+            setDataFetched(true);
+        }
+        fetchTodo();
+    },[]);
+
+
+    const handleCompleted = () => {
+        setIsCompleted(!isCompleted);
+    }
+
+    const handleIncompleted = () => {
+        setIsIncompleted(!isIncompleted);
+    }
+    
+    return(
+        <>{dataFetched &&
+            <>
+                <ol>
+                    {
+                        fetchedData.map((obj) => {
+                            if(isCompleted && obj.completed === true && obj.id <= 20){
+                                return <li key={obj.id}>
+                                    <div className="todo" id={`todo-${obj.id}`}>
+                                        <div className="todo-text">{obj.title}</div>
+                                        <div className="todo-status">{'Completed'}</div>
+                                    </div>
+                                </li>
+                            }
+                            if(isIncompleted && obj.completed === false && obj.id <= 20){
+                                return <li key={obj.id}>
+                                    <div className="todo" id={`todo-${obj.id}`}>
+                                        <div className="todo-text">{obj.title}</div>
+                                        <div className="todo-status">{'Incomplete'}</div>
+                                    </div>
+                                </li>
+                            }
+                        })
+                    }
+                </ol>
+            
+                <div id="filter-holder">
+                    Show completed <input id="completed-checkbox" type={'checkbox'} value={'Show Complete'} checked={isCompleted} onChange={handleCompleted} />
+                    <br/>
+                    Show incompleted <input id="incompleted-checkbox" type={'checkbox'} value={'Show Complete'} checked={isIncompleted} onChange={handleIncompleted}/>
+                </div>
+            </>
+            }          
+        </>
     )
-}
+ }
